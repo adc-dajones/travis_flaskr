8684a67fba56e345a48d21092fdccaab2151bf86
Dave Jones <dajones@auction.com>
2015-09-24 22:17:30 -0700

    changing python v2.6 to v2.7 required by sauce client

--- djsetup.sh
+++ djsetup.sh
@@ -1,14 +1,25 @@
 sudo pip install virtualenv
-mkdir py266
-cd py266
-wget https://www.python.org/ftp/python/2.6.6/Python-2.6.6.tgz
-tar -xvzf Python-2.6.6.tgz
-cd Python-2.6.6
+
+# mkdir py266
+# cd py266
+# wget https://www.python.org/ftp/python/2.6.6/Python-2.6.6.tgz
+# tar -xvzf Python-2.6.6.tgz
+# cd Python-2.6.6
+
+mkdir py276
+cd py276
+wget https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tgz
+tar -xvzf Python-2.7.6.tgz
+cd Python-2.7.6
+
 ./configure --prefix=`pwd`/..
 make
 make install
 cd ../..
-virtualenv venv --no-site-packages --python=`pwd`/py266/bin/python
+
+# virtualenv venv --no-site-packages --python=`pwd`/py266/bin/python
+virtualenv venv --no-site-packages --python=`pwd`/py276/bin/python
+
 source venv/bin/activate
 sudo pip install -r requirements.txt
 sudo pip install --upgrade https://github.com/mitsuhiko/flask/tarball/master
