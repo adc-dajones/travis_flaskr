6d4bbe56c119e17ec560ec4db8b7a21997309b5a
Dave Jones <dajones@auction.com>
2015-09-24 17:23:00 -0700

    updates to setup process

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # travis_flaskr
 TravisCI flaskr sample app
+
+Setup:
+$> sh -x djsetup.sh
+$> venv
+$> which python; python -V
+
+Run:
+$> python hello.py
+browser> http://localhost:5000
+^C when done to kill the server.
--- djsetup.sh
+++ djsetup.sh
@@ -1,12 +1,14 @@
-pip install virtualenv
+sudo pip install virtualenv
 mkdir py266
 cd py266
-https://www.python.org/ftp/python/2.6.6/Python-2.6.6.tgz
+wget https://www.python.org/ftp/python/2.6.6/Python-2.6.6.tgz
 tar -xvzf Python-2.6.6.tgz
 cd Python-2.6.6
-./configure --prefix=`pwd`
+./configure --prefix=`pwd`/..
 make
 make install
-virtualenv venv --no-site-packages --python=`pwd`/bin/python
+cd ../..
+virtualenv venv --no-site-packages --python=`pwd`/py266/bin/python
 source venv/bin/activate
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
+alias venv='source venv/bin/activate'
